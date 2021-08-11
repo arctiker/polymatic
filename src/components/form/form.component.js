f707b94a6584aa4de0a3b20c8712d5463dc65229
Ibrahim <ibrahim.bawab@hotmail.com>
2021-08-12 00:11:55 +0300

    first ahmad

--- src/components/form/form.component.js
+++ src/components/form/form.component.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import './form.styles.scss';
 
+
 function Form() {
     return (
-        <div className='polymatic'>
-            <div>
-                <form>
-                    <fieldset className=''>
-                        <h2 className='title1'>Get a Free Demo.<br></br>it's Completely Free!</h2>
-                            <div className='container1'>
-                                <h4>Name*</h4>
-                                    <input  className='input' type='text' name='name' placeholder='Enter your Name here' required />
-                                <h4>Address</h4>
-                                    <input className='input' type='text' name='address' placeholder='Enter your Address here' required />
-                                <h4>Phone Number</h4>
-                                    <input className='input' type='tel' name='number's required/>
-                                <h4>Select Demo Time</h4>
-                                    <div className='container2'>                                <button className='btn2'>1-2</button>
-                                        <button className='btn2'>3-4</button>
-                                        <button className='btn2'>5-6</button>
-                                        <button className='btn2'>7-8</button>
-                                    </div>
-                                <h4>Where did you hear about us?</h4>
-                                        <input className='input' type='dropbox' placeholder='select' name='name' required /> <br></br>
-                                    <div>
-                                        <button className='btn3'>Continue</button> 
-                                    </div>
-       
-                                <p>By registering here, I agree to PolyMatic’s Terms & Conditions and Privacy Policy</p>
-                            </div>
-                    </fieldset>
-                </form>
-            </div>
-        </div>
+            <form className='form'>
+                <h2 className='title1'>Get a Free Demo.<br/>it's Completely <span style={{color:'blue'}}>Free!</span></h2>
+                <div className='container'>
+                    <label className='label'>Name<span style={{color:'red'}}>*</span></label>
+                        <input  className='input' type='text' name='name' placeholder='Enter your Name here' required />
+                    <label className='label'>Address<span style={{color:'red'}}>*</span></label>
+                        <input className='input' type='text' name='address' placeholder='Enter your Address here' required />
+                    <label className='label'>Phone Number<span style={{color:'red'}}>*</span></label>
+                        <input className='input' type='tel' name='number's required/>
+                    <label className='label'>Select Demo Time<span style={{color:'red'}}>*</span></label>
+                    <div className='container1'>
+                        <button className='btn2'>1-2</button>
+                        <span>
+                            <button className='btn2'>3-4</button>
+                        </span>
+                        <button className='btn2'>5-6</button>
+                        <button className='btn2'>7-8</button>
+                    </div>
+                    <label className='label'>Where did you hear about us?</label>
+                        <select className='input' type='dropbox' placeholder='select' required>
+                            <option value='ahmad'>ahmad</option>  
+                            <option value='luai'>luai</option>
+                            <option value='ali'>ali</option>
+                            <option value='bob'>bob</option>
+                        </select>
+                        <button className='btn3'>Continue</button> 
+                        <p>By registering here, I agree to PolyMatic’s <span style={{color:'blue'}}>Terms & Conditions</span> and <span style={{color:'blue'}}>Privacy Policy</span></p>
+                </div>
+            </form>
     )
 }
 
--- src/components/popup/popup.component.js
+++ src/components/popup/popup.component.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './popup.styles.scss';
-import { ReactComponent as Logo } from '../images/close-btn.svg';
+import { ReactComponent as Logo } from '../images/Vector.svg';
+import { ReactComponent as Logo1 } from '../images/Vector(1).svg';
+
 
 function Popup(props) {
     return (props.trigger) ? (
-        <div className='popup'>
-            <div className='popup-inner'>
-                <button className='close-btn' onClick={() => props.setTrigger(false)}><Logo /></button>
-                { props.children }
+        <div className='popup'>  
+            <div className='popup-left'>
+                <Logo1 />
+                    <label className='label1'>
+                        PolyMatic
+                    </label> 
+                    <span className='span'>Redesign Everything</span>
+            </div>
+
+            <div className='popup-right'>
+            <button className='close-btn' onClick={() => props.setTrigger(false)}><Logo /></button>
+            { props.children }
             </div>
         </div>
+
     ): '';
 }
 
